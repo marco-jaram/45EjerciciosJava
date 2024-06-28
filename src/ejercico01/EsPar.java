31364bce31b9c3c15a84894df9559e43653a3366
Marco Jaramillo <marco.jaramillocast@gmail.com>
2024-06-27 18:19:17 -0700

    ejercicio01

---
+++ src/ejercico01/EsPar.java
@@ -0,0 +1,15 @@
+package ejercico01;
+
+public class EsPar {
+
+
+    public static void main(String[] args) {
+        int num=101;
+        if ((num % 2) == 0)
+            System.out.println("El numero introducido es PAR");
+        else
+            System.out.println("El numero introducido es IMPAR");
+    }
+
+}
+
