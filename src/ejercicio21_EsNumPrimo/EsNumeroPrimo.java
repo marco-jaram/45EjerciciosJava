0cda92cc7f748e7c2043ac7e12d4896ff9b78825
Marco Jaramillo <marco.jaramillocast@gmail.com>
2024-07-24 10:02:52 -0700

    add ejercicios20

---
+++ src/ejercicio21_EsNumPrimo/EsNumeroPrimo.java
@@ -0,0 +1,33 @@
+package ejercicio21_EsNumPrimo;
+
+import java.util.Scanner;
+
+public class EsNumeroPrimo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un número: ");
+        int numero = scanner.nextInt();
+
+        if (esPrimo(numero)) {
+            System.out.println(numero + " es primo.");
+        } else {
+            System.out.println(numero + " no es primo.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean esPrimo(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
